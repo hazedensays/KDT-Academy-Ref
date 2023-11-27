997f8d04c8896f1dab1c3149d004b94b4b30ce32
HAZE\SAMSUNG <hazedensays@gmail.com>
2023-11-27 21:48:11 +0900

    20231127

--- java/myWork_java/demoJ01/src/main/java/com/example/demo/controller/RTestController.java
+++ java/myWork_java/demoJ01/src/main/java/com/example/demo/controller/RTestController.java
@@ -15,8 +15,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @AllArgsConstructor
 public class RTestController {
-	
-	
+
 	@GetMapping("/checkdata")
 	public String hello() {
 		log.info("** React Connect Test 중~");
